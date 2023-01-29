c528f166f60882592f085887f40d4a9ccf9f9f09
Jules Hedges <jules.hedges@strath.ac.uk>
2023-01-29 15:27:10 +0000

    vL encoding of monadic lenses

---
+++ src/Cybercat/Sketches/MonadicLens.hs
@@ -0,0 +1,31 @@
+{-# LANGUAGE RankNTypes #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE IncoherentInstances #-}
+
+module Cybercat.Sketches.MonadicLens where
+
+import Control.Monad
+import Data.Functor.Const
+import Control.Lens
+
+class RightModule m f where
+    act :: f (m a) -> f a
+
+instance (Monad m) => RightModule m m where
+    act = join
+
+-- We need IncoherentInstances because (Const a) is also a Monad
+instance RightModule m (Const a) where
+    act = Const . getConst
+
+type MonadicLens m s t a b = forall f. (Functor f, RightModule m f) => LensLike f s t a b
+
+monadicGet :: MonadicLens m s t a b -> s -> a
+monadicGet l s = getConst (l (Const) s)
+
+monadicPut :: (Monad m) => MonadicLens m s t a b -> s -> b -> m t
+monadicPut l s b = l (const (return b)) s
+
+monadicLens :: (Monad m) => (s -> a) -> (s -> b -> m t) -> MonadicLens m s t a b
+monadicLens g p k s = act (fmap (p s) (k (g s)))
